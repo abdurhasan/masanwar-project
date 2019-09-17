7bdf026a68596a0ed7fc5490a7297a667bdfe61c
achan <abdurhasan20@gmail.com>
2019-09-17 13:52:23 +0700

    yoo

--- main.js
+++ main.js
@@ -8,6 +8,13 @@ const Authorization = { headers: { 'Authorization': "bearer " + token } };
 let data = new Array;
 let Employeedoc = new Array;
 let EmployeeIndex;
+let Locationdoc = new Array;
+
+
+
+
+
+
 
 $(".dropdown-menu").on("click", "a", function (e) {
   e.preventDefault();
@@ -43,7 +50,7 @@ const renderEmployee = () => {
     return $('#employee_form > .row > ul').remove()
   };
   $('#employee_form > .row ').append(`
-  <button name="employee-${EmployeeIndex}" id="employee-${EmployeeIndex}" style="background-color: white !important; color: grey" class="col-md-2 btn btn-primary dropdown-toggle" type="button"  id="employee" data-toggle="dropdown" > 
+  <button name="employee-${EmployeeIndex}" id="employee-${EmployeeIndex}" style="background-color: white !important; color: grey" class="col-md-2 btn btn-primary dropdown-toggle" type="button"  data-toggle="dropdown" > 
   Employees <span class="caret"></span> </button> 
    <ul class="dropdown-menu"></ul>`);
 
@@ -56,34 +63,16 @@ const renderEmployee = () => {
 
 }
 
-function updateUser(el) {
-  const Target = $(el).attr("data-id");
-  const Datums = $(`#form-${Target}`)['0']
-  let stateData = new Object;
 
-  for(let el of Datums){
-    stateData[el.name] = el.value
-  }
-    console.log(stateData)
-  axios.put(`${BASE_URL}/inventories/${Target}`, stateData, Authorization)
-      .then(
-        snap=>console.log(snap)
-      )
 
-}
-
-$("form").submit(async e => {
+$("#mainForm").submit(async e => {
   e.preventDefault();
   const invNo = await axios.get(BASE_URL + '/inventories/count', Authorization).then(s => s.data)
   const Target = e.target
   let stateData = {
     inventory_number: invNo + 1
   }
-  
   let employeeDoc = new Array;
-
-  
-
   for (let i = 0; i < Target.length - 1; i++) {
     const currentValue = Target[i].value;
     const fixValue = isNaN(Target[i].value) ? Target[i].value : Number(currentValue);
@@ -93,21 +82,13 @@ $("form").submit(async e => {
     } else {
       stateData[Target[i].name] = fixValue;
     }
-
-
   }
-
   stateData['employees'] = employeeDoc;
-
-
-  // axios.post(BASE_URL + '/inventories', stateData, Authorization)
-  //   .then(snap => {
-  //     console.log(snap)
-  //     appendToUsrTable(snap.data)
-  //     flashMessage(true,'Data berhasil ditambahkan')
+  console.log(stateData)
+  // axios.post(BASE_URL + '/inventories', Authorization)
+  //   .then(res => {
+  //     console.log(res)
   //   })
-  // .catch(_=>flashMessage(false,'Gagal menambahkan data , cek kembali inputan anda'))
-
 
 });
 
@@ -120,17 +101,12 @@ axios.get(BASE_URL + '/employees', Authorization)
     EmployeeIndex = snap.data.length;
     renderEmployee()
   })
-
-
-
-
 axios.get(BASE_URL + '/inventorygroups', Authorization)
   .then(snap => {
     $.each(snap.data, (index, value) => {
       $('#inventorygroup_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
     });
   })
-
 axios.get(BASE_URL + '/divisions', Authorization)
   .then(snap => {
     $.each(snap.data, (index, value) => {
@@ -139,6 +115,7 @@ axios.get(BASE_URL + '/divisions', Authorization)
   })
 axios.get(BASE_URL + '/locations', Authorization)
   .then(snap => {
+    Locationdoc.push(...snap.data)
     $.each(snap.data, (index, value) => {
       $('#location_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
     });
@@ -156,8 +133,6 @@ axios.get(BASE_URL + '/branches', Authorization)
       $('#branch_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
     });
   })
-
-
 axios.get(BASE_URL + '/suppliers', Authorization)
   .then(snap => {
 
@@ -168,19 +143,11 @@ axios.get(BASE_URL + '/suppliers', Authorization)
 
 
   })
-
 axios.get(BASE_URL + '/inventories', Authorization)
   .then(snap => {
     data.push(...snap.data)
     renderPage()
   })
-
-
-
-
-
-
-
 function appendToUsrTable(i, snap) {
   let invno = ''
   if (snap.inventorygroup.nomor) { invno += `${snap.inventorygroup.nomor}/` } else invno += '--/'
@@ -190,39 +157,41 @@ function appendToUsrTable(i, snap) {
   $("#userTable > tbody:last-child").append(`
           <tr>
               <td  name="inventory_number">${invno}</td>
-              '<td  name="name">${snap.name}</td>
-              '<td  name="qty">${snap.qty}</td>              
-              '<td  name="status">${snap.status}</td>
-              '<td  name="condition">${snap.condition}</td>              
-              '<td  name="location">${snap.location.name}</td>
-              '<td align="center">
+              <td  name="name">${snap.name}</td>
+              <td  name="qty">${snap.qty}</td>              
+              <td  name="status">${snap.status}</td>
+              <td  name="condition">${snap.condition}</td>              
+              <td  name="location">${snap.location.name}</td>
+              <td align="center">
               <button class="btn btn-success form-control" id="${snap.id}" onClick="editInventory(this)" data-toggle="modal" data-target="#myModal")">EDIT</button>
               </td>              
           </tr>
       `);
 }
-
-
-
-
-
 function flashMessage(status, msg) {
   $(".flashMsg").remove();
   if (status) {
     $("#head").prepend(`
-          <div class="col-sm-12"><div class="flashMsg alert alert-success alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true" class="glyphicon glyphicon-remove"></span></button> <strong>${msg}</strong></div></div>
+          <div id="msg" class="col-sm-12"><div class="flashMsg alert alert-success alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true" class="glyphicon glyphicon-remove"></span></button> <strong>${msg}</strong></div></div>
       `);
+    setTimeout(_ => $("#msg").remove(), 1500)
   } else {
     $("#head").prepend(`
-          <div class="col-sm-12"><div class="flashMsg alert alert-danger alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button> <strong>${msg}</strong></div></div>
+          <div id="msg" class="col-sm-12"><div class="flashMsg alert alert-danger alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button> <strong>${msg}</strong></div></div>
       `);
+    setTimeout(_ => $("#msg").remove(), 1500)
   }
 }
-
-
 function editInventory(el) {
   const invID = el.id;
-  const localdata = data.filter(jq => jq.id == invID)[0]
+  let Index;
+  let localdata;
+  data.map((jq, index) => {
+    if (jq.id == invID) {
+      localdata = jq
+      Index = index;
+    }
+  })[0]
   $(".modal-body").empty().append(`
   <form id="form-${invID}">      
       
@@ -233,26 +202,88 @@ function editInventory(el) {
       <input class="form-control" type="number" name="qty" value="${localdata.qty}"/>
       
       <label for="status">Status</label>
-      <input class="form-control" type="text" name="status" value="${localdata.status}"/>
+      <input class="form-control" onchange="elEnum(this,'status')" type="text" value="${localdata.status}"/>
 
       <label for="condition">Condition</label>
       <input class="form-control" type="text" name="condition" value="${localdata.condition}"/>
-
-      <label for="location">Location</label>
-      <input class="form-control" type="text" name="location" value="${localdata.location.name}"/>
+            
+      <label for="locationedit-${invID}">Location</label>
+      <div class="form-group dropdown" id="locationedit">
+      <button style="text-indent:-50%;background-color: white !important; color: grey"
+          class="form-control btn btn-primary  dropdown-toggle" type="button" name="location"
+          data-toggle="dropdown" id="locationedit-${invID}"
+          value="${localdata.location.id}"
+          >
+          ${localdata.location.name}
+          <span class="caret"></span>
+      </button>
+      <ul class="dropdown-menu">
+      ${Locationdoc.map(snap => {
+    return `<li><a href="#" onclick="Memantapkan(this)" data-parent="${invID}"  data-id="${snap.id}">` + snap.name + '</a></li>'
+  })}            
+      </ul>
+  </div>
 
 
   </form>
 `);
-
-
-
   $(".modal-footer").empty().append(`
-                <form>
-                    <button type="button" type="submit" class="btn btn-primary" onClick="updateUser(this)" data-id="${invID}">Save changes</button>
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                </form>
-            `);
+        <form>
+            <button type="button" type="submit" class="btn btn-primary" data-index="${Index}" onClick="updateUser(this)" data-id="${invID}">Save changes</button>
+            <button type="button" class="btn btn-default" data-dismiss="modal" id="closeedit">Close</button>
+        </form>
+    `);
+}
+function updateUser(el) {
+  const Target = $(el).data("id");
+  const Index = $(el).data("index");
+  const Datums = $(`#form-${Target}`)['0']
+  let stateData = new Object;
+
+  for (let el of Datums) {
+    stateData[el.name] = el.value
+  }
+
+  axios.put(`${BASE_URL}/inventories/${Target}`, stateData, Authorization)
+    .then(
+      snap => {
+        flashMessage(true, "data berhasil diupdate")
+        $('#closeedit').trigger("click")
+        data[Index] = snap.data
+        renderPage()
+      }
+    )
+    .catch(snap => {
+      flashMessage(false, "data gagal diupdate, cek koneksi internet anda")
+      $('#closeedit').trigger("click")
+    })
+
 
 }
 
+function Memantapkan(el) {
+
+  const thisTxt = $(el).text();
+  const thisId = $(el).data("id");
+  const parent = $(el).data("parent");
+  const display = $(`#locationedit-${parent}`);
+
+  display.text(thisTxt)
+  display.val(thisId)
+}
+
+
+function elEnum(el, type) {
+  const currentValue = $(el).val().toLowerCase()
+  if (type == 'status') {
+    if (currentValue !== 'buy' && currentValue !== 'grant') {
+      alert('Status hanya : buy atau grant')
+      $(el).val('')
+    }
+  } else {
+    if (currentValue !== 'good' && currentValue !== 'bad') {
+      alert('Condition hanya : good atau bad')
+      $(el).val('')
+    }
+  }
+}
