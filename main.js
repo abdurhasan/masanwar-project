4a7bde13e35dbdbc015fef62e194094a06902f91
achanDev <30721850+abdurhasan@users.noreply.github.com>
2019-09-24 22:53:40 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,198 +1,143 @@
-// const userData = JSON.parse(localStorage.getItem('user'));
 const BASE_URL = 'http://localhost:1337';
 const token = localStorage.getItem('token');
-const Authorization = { headers: { 'Authorization': "bearer " + token } };
-
-
-
+if (!token) document.location.href = 'index.html';
+axios.defaults.headers.common.Authorization = "bearer " + token;
 let data = new Array;
 let Employeedoc = new Array;
 let EmployeeIndex;
 let Locationdoc = new Array;
-
-
-
-
-
-
-
 $(".dropdown-menu").on("click", "a", function (e) {
-  e.preventDefault();
-  const $this = $(this);
-  const display = $this.parents().eq(1).siblings('button');
-  display.text($this.text());
-
-  display.val($this.data("id"));
-
+	e.preventDefault();
+	const $this = $(this);
+	const display = $this.parents().eq(1).siblings('button');
+	display.text($this.text());
+	display.val($this.data("id"))
 });
-const renderPage = _ => {
-  $("#userTable > tbody").empty()
-  $.each(data, (i, inv) => {
-    appendToUsrTable(i, inv);
-  });
+
+function renderPage() {
+	$("#userTable > tbody").empty()
+	return $.each(data, (i, inv) => {
+		appendToUsrTable(i, inv)
+	})
 }
 $("#employee_form").on("click", "a", function (e) {
-  e.preventDefault();
-  const $this = $(this);
-  const $text = $this.text();
-  const $id = $this.data("id");
-  const display = $(`#employee-${EmployeeIndex}`);
-
-  display.text($text);
-  display.val($id);
-  Employeedoc = Employeedoc.filter(el => el.id !== $id);
-  EmployeeIndex = EmployeeIndex - 1;
-  renderEmployee()
+	e.preventDefault();
+	const $this = $(this);
+	const $text = $this.text();
+	const $id = $this.data("id");
+	const display = $(`#employee-${EmployeeIndex}`);
+	display.text($text);
+	display.val($id);
+	Employeedoc = Employeedoc.filter(el => el.id !== $id);
+	EmployeeIndex = EmployeeIndex - 1;
+	renderEmployee()
 });
-
 const renderEmployee = () => {
-  if (Employeedoc.length < 1) {
-    return $('#employee_form > .row > ul').remove()
-  };
-  $('#employee_form > .row ').append(`
+	if (Employeedoc.length < 1) {
+		return $('#employee_form > .row > ul').remove()
+	};
+	$('#employee_form > .row ').append(`
   <button name="employee-${EmployeeIndex}" id="employee-${EmployeeIndex}" style="background-color: white !important; color: grey" class="col-md-2 btn btn-primary dropdown-toggle" type="button"  data-toggle="dropdown" > 
   Employees <span class="caret"></span> </button> 
    <ul class="dropdown-menu"></ul>`);
-
-  $('#employee_form > .row > ul > li').remove();
-
-  $.each(Employeedoc, (i, value) => {
-    $('#employee_form > .row > ul').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
-  });
-
-
+	$('#employee_form > .row > ul > li').remove();
+	$.each(Employeedoc, (i, value) => {
+		$('#employee_form > .row > ul').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`)
+	})
 }
+axios.get(BASE_URL + '/employees').then(snap => {
+	Employeedoc.push(...snap.data)
+	EmployeeIndex = snap.data.length;
+	renderEmployee()
+})
+axios.get(BASE_URL + '/inventorygroups').then(snap => {
+	$.each(snap.data, (index, value) => {
+		$('#inventorygroup_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`)
+	})
+})
+axios.get(BASE_URL + '/divisions').then(snap => {
+	$.each(snap.data, (index, value) => {
+		$('#division_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`)
+	})
+})
+axios.get(BASE_URL + '/locations').then(snap => {
+	Locationdoc.push(...snap.data)
+	$.each(snap.data, (index, value) => {
+		$('#location_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`)
+	})
+})
+axios.get(BASE_URL + '/brands').then(snap => {
+	$.each(snap.data, (index, value) => {
+		$('#brand_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`)
+	})
+})
+axios.get(BASE_URL + '/branches').then(snap => {
+	$.each(snap.data, (index, value) => {
+		$('#branch_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`)
+	})
+})
+axios.get(BASE_URL + '/suppliers').then(snap => {
+	$.each(snap.data, (index, value) => {
+		$('#supplier_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`)
+	})
+})
+axios.get(BASE_URL + '/inventories').then(snap => {
+	data.push(...snap.data)
+	renderPage()
+})
 
-
-
-$("#mainForm").submit(async e => {
-  e.preventDefault();
-  const invNo = await axios.get(BASE_URL + '/inventories/count', Authorization).then(s => s.data)
-  const Target = e.target
-  let stateData = {
-    inventory_number: invNo + 1
-  }
-  let employeeDoc = new Array;
-  for (let i = 0; i < Target.length - 1; i++) {
-    const currentValue = Target[i].value;
-    const fixValue = isNaN(Target[i].value) ? Target[i].value : Number(currentValue);
-
-    if (Target[i].name.includes('employee')) {
-      if (Target[i].value) employeeDoc.push(Target[i].value)
-    } else {
-      stateData[Target[i].name] = fixValue;
-    }
-  }
-  stateData['employees'] = employeeDoc;
-  console.log(stateData)
-  // axios.post(BASE_URL + '/inventories', Authorization)
-  //   .then(res => {
-  //     console.log(res)
-  //   })
-
-});
-
-
-
-
-axios.get(BASE_URL + '/employees', Authorization)
-  .then(snap => {
-    Employeedoc.push(...snap.data)
-    EmployeeIndex = snap.data.length;
-    renderEmployee()
-  })
-axios.get(BASE_URL + '/inventorygroups', Authorization)
-  .then(snap => {
-    $.each(snap.data, (index, value) => {
-      $('#inventorygroup_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
-    });
-  })
-axios.get(BASE_URL + '/divisions', Authorization)
-  .then(snap => {
-    $.each(snap.data, (index, value) => {
-      $('#division_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
-    });
-  })
-axios.get(BASE_URL + '/locations', Authorization)
-  .then(snap => {
-    Locationdoc.push(...snap.data)
-    $.each(snap.data, (index, value) => {
-      $('#location_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
-    });
-  })
-axios.get(BASE_URL + '/brands', Authorization)
-  .then(snap => {
-    $.each(snap.data, (index, value) => {
-      $('#brand_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
-    });
-  })
-axios.get(BASE_URL + '/branches', Authorization)
-  .then(snap => {
-    $.each(snap.data, (index, value) => {
-
-      $('#branch_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
-    });
-  })
-axios.get(BASE_URL + '/suppliers', Authorization)
-  .then(snap => {
-
-    $.each(snap.data, (index, value) => {
-
-      $('#supplier_form > ul ').append(`<li><a href="#" data-id="${value.id}">${value.name}</a></li>`);
-    });
-
-
-  })
-axios.get(BASE_URL + '/inventories', Authorization)
-  .then(snap => {
-    data.push(...snap.data)
-    renderPage()
-  })
 function appendToUsrTable(i, snap) {
-  let invno = ''
-  if (snap.inventorygroup.nomor) { invno += `${snap.inventorygroup.nomor}/` } else invno += '--/'
-  if (snap.branch) { invno += `${snap.branch.code}/` } else invno += '--/'
-  invno += snap.inventory_number
-
-  $("#userTable > tbody:last-child").append(`
+	let invno = ''
+	let locationname = ''
+	if (snap.inventorygroup) {
+		invno += `${snap.inventorygroup.nomor}/`
+	} else invno += '--/'
+	if (snap.branch) {
+		invno += `${snap.branch.code}/`
+	} else invno += '--/'
+	if (snap.location) locationname = snap.location.name
+	invno += snap.inventory_number
+	$("#userTable > tbody:last-child").append(`
           <tr>
               <td  name="inventory_number">${invno}</td>
               <td  name="name">${snap.name}</td>
               <td  name="qty">${snap.qty}</td>              
               <td  name="status">${snap.status}</td>
               <td  name="condition">${snap.condition}</td>              
-              <td  name="location">${snap.location.name}</td>
+              <td  name="location">${locationname}</td>
               <td align="center">
               <button class="btn btn-success form-control" id="${snap.id}" onClick="editInventory(this)" data-toggle="modal" data-target="#myModal")">EDIT</button>
               </td>              
           </tr>
-      `);
+      `)
 }
+
 function flashMessage(status, msg) {
-  $(".flashMsg").remove();
-  if (status) {
-    $("#head").prepend(`
+	$(".flashMsg").remove();
+	if (status) {
+		$("#head").prepend(`
           <div id="msg" class="col-sm-12"><div class="flashMsg alert alert-success alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true" class="glyphicon glyphicon-remove"></span></button> <strong>${msg}</strong></div></div>
       `);
-    setTimeout(_ => $("#msg").remove(), 1500)
-  } else {
-    $("#head").prepend(`
+		setTimeout(_ => $("#msg").remove(), 1500)
+	} else {
+		$("#head").prepend(`
           <div id="msg" class="col-sm-12"><div class="flashMsg alert alert-danger alert-dismissible fade in" role="alert"> <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button> <strong>${msg}</strong></div></div>
       `);
-    setTimeout(_ => $("#msg").remove(), 1500)
-  }
+		setTimeout(_ => $("#msg").remove(), 1500)
+	}
 }
+
 function editInventory(el) {
-  const invID = el.id;
-  let Index;
-  let localdata;
-  data.map((jq, index) => {
-    if (jq.id == invID) {
-      localdata = jq
-      Index = index;
-    }
-  })[0]
-  $(".modal-body").empty().append(`
+	const invID = el.id;
+	let Index;
+	let localdata;
+	data.map((jq, index) => {
+		if (jq.id == invID) {
+			localdata = jq
+			Index = index
+		}
+	})[0]
+	$(".modal-body").empty().append(`
   <form id="form-${invID}">      
       
       <label for="name">Name</label>
@@ -219,7 +164,10 @@ function editInventory(el) {
       </button>
       <ul class="dropdown-menu">
       ${Locationdoc.map(snap => {
-    return `<li><a href="#" onclick="Memantapkan(this)" data-parent="${invID}"  data-id="${snap.id}">` + snap.name + '</a></li>'
+    return ` < li > < a href = "#"
+		onclick = "Memantapkan(this)"
+		data - parent = "${invID}"
+		data - id = "${snap.id}" > ` + snap.name + '</a></li>'
   })}            
       </ul>
   </div>
@@ -227,63 +175,82 @@ function editInventory(el) {
 
   </form>
 `);
-  $(".modal-footer").empty().append(`
+	$(".modal-footer").empty().append(`
         <form>
             <button type="button" type="submit" class="btn btn-primary" data-index="${Index}" onClick="updateUser(this)" data-id="${invID}">Save changes</button>
             <button type="button" class="btn btn-default" data-dismiss="modal" id="closeedit">Close</button>
         </form>
-    `);
+    `)
 }
+
 function updateUser(el) {
-  const Target = $(el).data("id");
-  const Index = $(el).data("index");
-  const Datums = $(`#form-${Target}`)['0']
-  let stateData = new Object;
-
-  for (let el of Datums) {
-    stateData[el.name] = el.value
-  }
-
-  axios.put(`${BASE_URL}/inventories/${Target}`, stateData, Authorization)
-    .then(
-      snap => {
-        flashMessage(true, "data berhasil diupdate")
-        $('#closeedit').trigger("click")
-        data[Index] = snap.data
-        renderPage()
-      }
-    )
-    .catch(snap => {
-      flashMessage(false, "data gagal diupdate, cek koneksi internet anda")
-      $('#closeedit').trigger("click")
-    })
-
-
+	const Target = $(el).data("id");
+	const Index = $(el).data("index");
+	const Datums = $(`#form-${Target}`)['0']
+	let stateData = new Object;
+	for (let el of Datums) {
+		stateData[el.name] = el.value
+	}
+	axios.put(`${BASE_URL}/inventories/${Target}`, stateData).then(snap => {
+		flashMessage(!0, "data berhasil diupdate")
+		$('#closeedit').trigger("click")
+		data[Index] = snap.data
+		renderPage()
+	}).catch(snap => {
+		flashMessage(!1, "data gagal diupdate, cek koneksi internet anda")
+		$('#closeedit').trigger("click")
+	})
 }
+$("#mainForm").submit(async e => {
+	e.preventDefault();
+	const invNo = await axios.get(BASE_URL + '/inventories/count').then(s => s.data)
+	const Target = e.target
+	let stateData = {
+		inventory_number: invNo + 1
+	}
+	let employeeDoc = new Array;
+	for (let i = 0; i < Target.length - 1; i++) {
+		const currentValue = Target[i].value;
+		const fixValue = isNaN(Target[i].value) ? Target[i].value : Number(currentValue);
+		if (Target[i].name.includes('employee')) {
+			if (Target[i].value) employeeDoc.push(Target[i].value)
+		} else {
+			stateData[Target[i].name] = fixValue
+		}
+	}
+	stateData.employees = employeeDoc;
+	axios.post(BASE_URL + '/inventories', stateData).then(res => {
+		flashMessage(!0, "data berhasil disubmit")
+		$('#mainForm').find('input').val('');
+		appendToUsrTable(null, res.data)
+	})
+});
 
-function Memantapkan(el) {
-
-  const thisTxt = $(el).text();
-  const thisId = $(el).data("id");
-  const parent = $(el).data("parent");
-  const display = $(`#locationedit-${parent}`);
-
-  display.text(thisTxt)
-  display.val(thisId)
+function
+Memantapkan(el) {
+	const thisTxt = $(el).text();
+	const thisId = $(el).data("id");
+	const parent = $(el).data("parent");
+	const display = $(`#locationedit-${parent}`);
+	display.text(thisTxt)
+	display.val(thisId)
 }
-
 
 function elEnum(el, type) {
-  const currentValue = $(el).val().toLowerCase()
-  if (type == 'status') {
-    if (currentValue !== 'buy' && currentValue !== 'grant') {
-      alert('Status hanya : buy atau grant')
-      $(el).val('')
-    }
-  } else {
-    if (currentValue !== 'good' && currentValue !== 'bad') {
-      alert('Condition hanya : good atau bad')
-      $(el).val('')
-    }
-  }
+	const currentValue = $(el).val().toLowerCase()
+	if (type == 'status') {
+		if (currentValue !== 'buy' && currentValue !== 'grant') {
+			alert('Status hanya : buy atau grant')
+			$(el).val('')
+		}
+	} else {
+		if (currentValue !== 'good' && currentValue !== 'bad') {
+			alert('Condition hanya : good atau bad')
+			$(el).val('')
+		}
+	}
 }
+$('#logout').click(e => {
+	localStorage.setItem('token', '');
+	document.location.href = 'index.html';
+})
